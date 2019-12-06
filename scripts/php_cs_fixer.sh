2796a54d558b8e5564394b7ebeaa74a9de0c2ca5
ykawa <ykawa@flugels.redirectme.net>
2019-12-06 14:53:19 +0900

    update script

--- scripts/php_cs_fixer.sh
+++ scripts/php_cs_fixer.sh
@@ -11,4 +11,4 @@ find .
 IFS=$'\n'
 COMMIT_SCA_FILES=($(git diff --name-only --diff-filter=ACMRTUXB origin/master ))
 unset IFS
-./vendor/bin/php-cs-fixer fix --config=.php_cs -vvv --dry-run --diff --path-mode=intersection "${COMMIT_SCA_FILES[@]}"
+./vendor/bin/php-cs-fixer fix --config=.php_cs.dist -vvv --dry-run --diff --path-mode=intersection "${COMMIT_SCA_FILES[@]}"
