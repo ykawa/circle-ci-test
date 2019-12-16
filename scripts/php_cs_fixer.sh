83aed058382dfba1f8321480047b332d3732683b
ykawa <ykawa@flugels.redirectme.net>
2019-12-16 16:26:56 +0900

    check environmets 2

--- scripts/php_cs_fixer.sh
+++ scripts/php_cs_fixer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 
 CIRCLECI_BRANCH_NAME="$1"
+CIRCLECI_RUNNING_ON_LOCAL="${2:-true}"
 BITBUCKET_BRANCH_NAME=$(git rev-parse --abbrev-ref @)
 
 echo "CIRCLECI BRANCH NAME: ${CIRCLECI_BRANCH_NAME}"
@@ -8,7 +9,11 @@ echo "BITBUCKET BRANCH NAME: ${BITBUCKET_BRANCH_NAME}"
 
 set +e
 IFS=$'\n'
-COMMIT_SCA_FILES=($(git diff -p --name-only --reverse --diff-filter=d --format="" origin/master...origin/${BITBUCKET_BRANCH_NAME} | grep \.php$ | sort | uniq))
+if [ "${CIRCLECI_RUNNING_ON_LOCAL}" = "true"]; then
+  COMMIT_SCA_FILES=($(git diff -p --name-only --reverse --diff-filter=d --format="" master...${BITBUCKET_BRANCH_NAME} | grep \.php$ | sort | uniq))
+else
+  COMMIT_SCA_FILES=($(git diff -p --name-only --reverse --diff-filter=d --format="" origin/master...origin/${BITBUCKET_BRANCH_NAME} | grep \.php$ | sort | uniq))
+fi
 unset IFS
 set -e
 
@@ -18,12 +23,12 @@ else
   echo "${COMMIT_SCA_FILES[@]}"
   echo ""
   ./vendor/bin/php-cs-fixer fix \
-	  --config=.php_cs.dist \
-	  --stop-on-violation \
-	  -vvv \
-	  --dry-run \
-	  --diff \
-	  --path-mode=intersection \
-	  "${COMMIT_SCA_FILES[@]}"
+    --config=.php_cs.dist \
+    --stop-on-violation \
+    -vvv \
+    --dry-run \
+    --diff \
+    --path-mode=intersection \
+    "${COMMIT_SCA_FILES[@]}"
 fi
 
